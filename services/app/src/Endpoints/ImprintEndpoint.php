4f3e31781cd56d5b22c90aea28ab1001389d2598
leoyn <leo@mixify.ga>
2020-12-09 01:38:18 +0100

    Implement imprint page, and enhance design

---
+++ services/app/src/Endpoints/ImprintEndpoint.php
@@ -0,0 +1,86 @@
+<?php
+
+require_once("HTMLEndpoint.php");
+
+class ImprintEndpoint extends HTMLEndpoint
+{
+    public function render()
+    {
+?>
+        <div class="wrapper">
+            <div class="slim">
+                <div class="slim-wrapper">
+
+
+                    <h1>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="76.672" height="42.114" viewBox="0 0 76.672 42.114">
+                            <g id="Component_3_2" data-name="Component 3 – 2" transform="translate(0.5 0.555)">
+                                <g id="Exclusion_3" data-name="Exclusion 3" transform="translate(1598 7943)">
+                                    <path d="M -1522.077758789062 -7901.70751953125 L -1522.344604492188 -7901.7255859375 L -1546.600341796875 -7903.3623046875 L -1546.833618164062 -7903.3779296875 L -1546.83349609375 -7903.61181640625 L -1546.833251953125 -7904.09521484375 L -1555.829833984375 -7904.9453125 L -1558.152221679688 -7904.08837890625 L -1558.1865234375 -7904.07568359375 L -1558.223022460938 -7904.0732421875 L -1569.344360351562 -7903.3720703125 L -1569.96337890625 -7901.8779296875 L -1570.02734375 -7901.7236328125 L -1570.1943359375 -7901.7236328125 L -1570.195190429688 -7901.7236328125 L -1573.061645507812 -7901.7236328125 L -1573.06982421875 -7901.7236328125 L -1573.078002929688 -7901.72412109375 L -1598.015991210938 -7903.3623046875 L -1598.249633789062 -7903.3779296875 L -1598.249633789062 -7903.61181640625 L -1598.249389648438 -7924.353515625 L -1598.249389648438 -7924.572265625 L -1598.032470703125 -7924.6015625 L -1582.195434570312 -7926.712890625 L -1582.195434570312 -7935.12841796875 L -1582.195434570312 -7935.32958984375 L -1581.9990234375 -7935.37255859375 L -1546.097290039062 -7943.244140625 L -1545.9462890625 -7943.27734375 L -1545.849243164062 -7943.1572265625 L -1540.794555664062 -7936.89794921875 L -1522.380126953125 -7940.84130859375 L -1522.077758789062 -7940.90576171875 L -1522.077758789062 -7940.5966796875 L -1522.077758789062 -7901.97509765625 L -1522.077758789062 -7901.70751953125 Z M -1568.847900390625 -7917.482421875 L -1568.847900390625 -7904.4248046875 L -1558.392944335938 -7905.39111328125 L -1558.392944335938 -7915.5341796875 L -1568.847900390625 -7917.482421875 Z M -1554.447021484375 -7906.33056640625 L -1547.190063476562 -7905.685546875 L -1547.190063476562 -7915.70947265625 L -1554.447021484375 -7915.29833984375 L -1554.447021484375 -7906.33056640625 Z M -1547.32763671875 -7921.7587890625 L -1547.318969726562 -7921.7587890625 L -1547.318359375 -7921.7587890625 L -1547.269165039062 -7921.7587890625 L -1547.223388671875 -7921.740234375 L -1543.300537109375 -7920.13525390625 L -1543.300537109375 -7932.5947265625 L -1547.391967773438 -7936.2470703125 L -1578.806762695312 -7930.04541015625 L -1578.806762695312 -7927.494140625 L -1572.630615234375 -7928.240234375 L -1572.579833984375 -7928.24658203125 L -1572.53076171875 -7928.232421875 L -1561.088134765625 -7924.90283203125 L -1560.907958984375 -7924.85009765625 L -1560.907958984375 -7924.66259765625 L -1560.907958984375 -7920.81591796875 L -1547.336303710938 -7921.75830078125 L -1547.32763671875 -7921.7587890625 Z" stroke="none" />
+                                    <path d="M -1570.1943359375 -7901.9736328125 L -1569.515625 -7903.61181640625 L -1558.23876953125 -7904.32275390625 L -1555.863037109375 -7905.19970703125 L -1546.583129882812 -7904.32275390625 L -1546.58349609375 -7903.61181640625 L -1522.327758789062 -7901.97509765625 L -1522.327758789062 -7940.5966796875 L -1540.892456054688 -7936.62109375 L -1546.043701171875 -7943 L -1581.945434570312 -7935.12841796875 L -1581.945434570312 -7926.494140625 L -1597.999389648438 -7924.353515625 L -1597.999633789062 -7903.61181640625 L -1573.061645507812 -7901.9736328125 L -1570.196166992188 -7901.9736328125 L -1570.195190429688 -7901.9736328125 L -1570.1943359375 -7901.9736328125 M -1569.097900390625 -7917.783203125 L -1569.098510742188 -7917.783203125 L -1569.097900390625 -7917.783203125 L -1569.097900390625 -7917.783203125 L -1558.142944335938 -7915.74169921875 L -1558.142944335938 -7905.1630859375 L -1569.097900390625 -7904.150390625 L -1569.097900390625 -7917.783203125 M -1546.940063476562 -7915.97412109375 L -1546.940063476562 -7905.412109375 L -1554.697021484375 -7906.10205078125 L -1554.697021484375 -7915.53466796875 L -1546.940063476562 -7915.97412109375 M -1547.318969726562 -7921.5087890625 L -1561.157958984375 -7920.5478515625 L -1561.157958984375 -7924.66259765625 L -1572.6005859375 -7927.9921875 L -1579.056762695312 -7927.2119140625 L -1579.056762695312 -7930.2509765625 L -1547.318359375 -7936.5166015625 L -1543.050537109375 -7932.70654296875 L -1543.050537109375 -7919.7626953125 L -1547.318115234375 -7921.5087890625 L -1547.318359375 -7921.5087890625 L -1547.318969726562 -7921.5087890625 M -1521.827758789062 -7901.4404296875 L -1522.361450195312 -7901.47607421875 L -1546.6171875 -7903.11279296875 L -1547.083740234375 -7903.14453125 L -1547.08349609375 -7903.6123046875 L -1547.083374023438 -7903.86767578125 L -1555.796752929688 -7904.69140625 L -1558.065673828125 -7903.853515625 L -1558.13427734375 -7903.828125 L -1558.207275390625 -7903.82373046875 L -1569.172973632812 -7903.13232421875 L -1569.732421875 -7901.7822265625 L -1569.860229492188 -7901.4736328125 L -1570.1943359375 -7901.4736328125 L -1570.195190429688 -7901.4736328125 L -1573.061645507812 -7901.4736328125 L -1573.078002929688 -7901.4736328125 L -1573.094360351562 -7901.474609375 L -1598.032348632812 -7903.11279296875 L -1598.499633789062 -7903.1435546875 L -1598.499633789062 -7903.61181640625 L -1598.499389648438 -7924.353515625 L -1598.499389648438 -7924.79150390625 L -1598.0654296875 -7924.84912109375 L -1582.445434570312 -7926.93212890625 L -1582.445434570312 -7935.12841796875 L -1582.445434570312 -7935.53076171875 L -1582.052490234375 -7935.61669921875 L -1546.150756835938 -7943.48828125 L -1545.848876953125 -7943.5546875 L -1545.654663085938 -7943.31396484375 L -1540.696533203125 -7937.17431640625 L -1522.432495117188 -7941.08544921875 L -1521.827758789062 -7941.21484375 L -1521.827758789062 -7940.5966796875 L -1521.827758789062 -7901.97509765625 L -1521.827758789062 -7901.4404296875 Z M -1568.597900390625 -7917.18115234375 L -1568.597900390625 -7904.69873046875 L -1558.642944335938 -7905.619140625 L -1558.642944335938 -7915.326171875 L -1568.597900390625 -7917.18115234375 Z M -1554.197021484375 -7906.5595703125 L -1547.440063476562 -7905.95849609375 L -1547.440063476562 -7915.44482421875 L -1554.197021484375 -7915.06201171875 L -1554.197021484375 -7906.5595703125 Z M -1547.336303710938 -7922.0087890625 L -1547.318969726562 -7922.0087890625 L -1547.318359375 -7922.0087890625 L -1547.219970703125 -7922.0087890625 L -1547.128784179688 -7921.9716796875 L -1543.550537109375 -7920.50732421875 L -1543.550537109375 -7932.482421875 L -1547.465698242188 -7935.97802734375 L -1578.556762695312 -7929.83984375 L -1578.556762695312 -7927.77587890625 L -1572.660522460938 -7928.48876953125 L -1572.55908203125 -7928.5009765625 L -1572.4609375 -7928.47216796875 L -1561.018310546875 -7925.142578125 L -1560.657958984375 -7925.0380859375 L -1560.657958984375 -7924.66259765625 L -1560.657958984375 -7921.083984375 L -1547.353637695312 -7922.0078125 L -1547.336303710938 -7922.0087890625 Z" stroke="none" />
+                                </g>
+                            </g>
+                        </svg>
+                        <span>Immo Inc.</span>
+                    </h1>
+                    <p class="light">
+                        An der Eiche 7,<br>
+                        22054 Hamburg
+                    </p>
+                    <p class="light">
+                        Geschäftsführerin: Anna Lustig
+                    </p>
+                    <div id="data-protection">
+                        <h2>Datenschutzerklärung</h2>
+                        <p><strong>Allgemeiner Hinweis und Pflichtinformationen</strong></p>
+                        <p><strong>Benennung der verantwortlichen Stelle</strong></p>
+                        <p>Die verantwortliche Stelle für die Datenverarbeitung auf dieser Website ist:</p>
+                        <p><span id="s3-t-firma">Immo Inc.</span><br><span id="s3-t-ansprechpartner">Anna Lustig</span><br><span id="s3-t-strasse">An der Eiche 7</span><br><span id="s3-t-plz">22054</span> <span id="s3-t-ort">Hamburg</span></p>
+                        <p></p>
+                        <p>Die verantwortliche Stelle entscheidet allein oder gemeinsam mit anderen über die Zwecke und Mittel der Verarbeitung von personenbezogenen Daten (z.B. Namen, Kontaktdaten o. Ä.).</p>
+
+                        <p><strong>Widerruf Ihrer Einwilligung zur Datenverarbeitung</strong></p>
+                        <p>Nur mit Ihrer ausdrücklichen Einwilligung sind einige Vorgänge der Datenverarbeitung möglich. Ein Widerruf Ihrer bereits erteilten Einwilligung ist jederzeit möglich. Für den Widerruf genügt eine formlose Mitteilung per E-Mail. Die Rechtmäßigkeit der bis zum Widerruf erfolgten Datenverarbeitung bleibt vom Widerruf unberührt.</p>
+
+                        <p><strong>Recht auf Beschwerde bei der zuständigen Aufsichtsbehörde</strong></p>
+                        <p>Als Betroffener steht Ihnen im Falle eines datenschutzrechtlichen Verstoßes ein Beschwerderecht bei der zuständigen Aufsichtsbehörde zu. Zuständige Aufsichtsbehörde bezüglich datenschutzrechtlicher Fragen ist der Landesdatenschutzbeauftragte des Bundeslandes, in dem sich der Sitz unseres Unternehmens befindet. Der folgende Link stellt eine Liste der Datenschutzbeauftragten sowie deren Kontaktdaten bereit: <a href="https://www.bfdi.bund.de/DE/Infothek/Anschriften_Links/anschriften_links-node.html" target="_blank">https://www.bfdi.bund.de/DE/Infothek/Anschriften_Links/anschriften_links-node.html</a>.</p>
+
+                        <p><strong>Recht auf Datenübertragbarkeit</strong></p>
+                        <p>Ihnen steht das Recht zu, Daten, die wir auf Grundlage Ihrer Einwilligung oder in Erfüllung eines Vertrags automatisiert verarbeiten, an sich oder an Dritte aushändigen zu lassen. Die Bereitstellung erfolgt in einem maschinenlesbaren Format. Sofern Sie die direkte Übertragung der Daten an einen anderen Verantwortlichen verlangen, erfolgt dies nur, soweit es technisch machbar ist.</p>
+
+                        <p><strong>Recht auf Auskunft, Berichtigung, Sperrung, Löschung</strong></p>
+                        <p>Sie haben jederzeit im Rahmen der geltenden gesetzlichen Bestimmungen das Recht auf unentgeltliche Auskunft über Ihre gespeicherten personenbezogenen Daten, Herkunft der Daten, deren Empfänger und den Zweck der Datenverarbeitung und ggf. ein Recht auf Berichtigung, Sperrung oder Löschung dieser Daten. Diesbezüglich und auch zu weiteren Fragen zum Thema personenbezogene Daten können Sie sich jederzeit über die im Impressum aufgeführten Kontaktmöglichkeiten an uns wenden.</p>
+
+                        <p><strong>SSL- bzw. TLS-Verschlüsselung</strong></p>
+                        <p>Aus Sicherheitsgründen und zum Schutz der Übertragung vertraulicher Inhalte, die Sie an uns als Seitenbetreiber senden, nutzt unsere Website eine SSL-bzw. TLS-Verschlüsselung. Damit sind Daten, die Sie über diese Website übermitteln, für Dritte nicht mitlesbar. Sie erkennen eine verschlüsselte Verbindung an der „https://“ Adresszeile Ihres Browsers und am Schloss-Symbol in der Browserzeile.</p>
+
+                        <p><strong>Server-Log-Dateien</strong></p>
+                        <p>In Server-Log-Dateien erhebt und speichert der Provider der Website automatisch Informationen, die Ihr Browser automatisch an uns übermittelt. Dies sind:</p>
+                        <ul>
+                            <li>Besuchte Seite auf unserer Domain</li>
+                            <li>Datum und Uhrzeit der Serveranfrage</li>
+                            <li>Browsertyp und Browserversion</li>
+                            <li>Verwendetes Betriebssystem</li>
+                            <li>Referrer URL</li>
+                            <li>Hostname des zugreifenden Rechners</li>
+                            <li>IP-Adresse</li>
+                        </ul>
+                        <p>Es findet keine Zusammenführung dieser Daten mit anderen Datenquellen statt. Grundlage der Datenverarbeitung bildet Art. 6 Abs. 1 lit. b DSGVO, der die Verarbeitung von Daten zur Erfüllung eines Vertrags oder vorvertraglicher Maßnahmen gestattet.</p>
+
+                        <p><strong>Cookies</strong></p>
+                        <p>Unsere Website verwendet Cookies. Das sind kleine Textdateien, die Ihr Webbrowser auf Ihrem Endgerät speichert. Cookies helfen uns dabei, unser Angebot nutzerfreundlicher, effektiver und sicherer zu machen. </p>
+                        <p>Einige Cookies sind “Session-Cookies.” Solche Cookies werden nach Ende Ihrer Browser-Sitzung von selbst gelöscht. Hingegen bleiben andere Cookies auf Ihrem Endgerät bestehen, bis Sie diese selbst löschen. Solche Cookies helfen uns, Sie bei Rückkehr auf
+                            unserer Website wiederzuerkennen.</p>
+                        <p>Mit einem modernen Webbrowser können Sie das Setzen von Cookies überwachen, einschränken oder unterbinden. Viele Webbrowser lassen sich so konfigurieren, dass Cookies mit dem Schließen des Programms von selbst gelöscht werden. Die Deaktivierung von Cookies
+                            kann eine eingeschränkte Funktionalität unserer Website zur Folge haben.</p>
+                        <p>Das Setzen von Cookies, die zur Ausübung elektronischer Kommunikationsvorgänge oder der Bereitstellung bestimmter, von Ihnen erwünschter Funktionen (z.B. Warenkorb) notwendig sind, erfolgt auf Grundlage von Art. 6 Abs. 1 lit. f DSGVO. Als Betreiber dieser
+                            Website haben wir ein berechtigtes Interesse an der Speicherung von Cookies zur technisch fehlerfreien und reibungslosen Bereitstellung unserer Dienste. Sofern die Setzung anderer Cookies (z.B. für Analyse-Funktionen) erfolgt, werden diese in dieser
+                            Datenschutzerklärung separat behandelt.</p>
+                        <p><small>Quelle: Datenschutz-Konfigurator von <a href="http://www.mein-datenschutzbeauftragter.de" target="_blank">mein-datenschutzbeauftragter.de</a></small></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+<?php
+    }
+}
